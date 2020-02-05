0591dc2a017cb5f5c79cdf28eb917a1cc606cad5
Hans <hansbel@yahoo.com>
2020-02-05 14:47:33 +0100

    Integrating old code into Laravel style

--- Hackathon/app/Http/Controllers/HomeController.php
+++ Hackathon/app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Conversation;
+use App\Services\KategorieDAO;
 
 class HomeController extends Controller
 {
@@ -23,6 +25,14 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+//        echo "Here?";
+//        Conversation::all());
+//        $conversations = Conversation::whereParentId(null)->orderBy(['conversation_id','id']);
+        $conversations = Conversation::orderBy('conversation_id')->orderBy('id')->get();
+//        dd($conversations);
+
+        $kategorieDAO = new KategorieDAO($conversations);
+    
+        return view('home', ['kategorieDAO' => $kategorieDAO]);
     }
 }
