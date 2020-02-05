4bdc4f8b6ab8d2427e4c535c808cd65a0f5a3a8f
Leonard Levering <l_levering@yahoo.com>
2020-02-05 16:25:20 +0100

    List support for sentences

--- Hackathon/resources/views/home.blade.php
+++ Hackathon/resources/views/home.blade.php
@@ -4,87 +4,11 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            {{-- Insert List Here --}}
-
-
-<?php
-use App\Services\KategorieDAO;
-
-//$kategorieDAO = new KategorieDAO();
-$kategorieArray = $kategorieDAO->getTreeByEventId('');
-$katIdAnzeige = "-1";
-
-?>
-            <table>
-                        <?php
-                        if (sizeof($kategorieArray) < 1) {
-                            echo"<tr><td>";
-                            ?>
-                            <a href='kategorieHinzufuegen.php?par=neuInHierarchie&beforeId=0&eventId=<?php echo $email ?? ''; ?>&insertLevel=0'>
-                                <img src='..\images\neu.png'>Neue Hauptkategorie
-                                <span class="glyphicon glyphicon-plus-sign"></span></a>
-                            <?php
-                            echo"</td></tr>";
-                        } else {
-                            for ($i = 0; $i < sizeof($kategorieArray); $i++) {
-                                $neu = 0;
-                                echo"<tr><td>";
-                                $katVO = $kategorieArray[$i];
-                                for ($j = 0; $j < ($katVO->getLevel() - 1); $j++) {
-                                    ?>
-                                    <img src='..\images\leer.png'><img src='..\images\leer.png'><img src='..\images\leer.png'><img src='..\images\leer.png'>
-                                    <?php
-                                }
-                                if ($katVO->getEndNode() === 1) {
-                                    ?>
-                                    <img src='..\images\leer.png'><img src='..\images\leer.png'><img src='..\images\leer.png'><img src='..\images\plus.png'>
-                                    <?php
-                                    $neu = 1;
-                                } else {
-                                    ?>
-                                    <img src='..\images\leer.png'><img src='..\images\leer.png'><img src='..\images\leer.png'><img src='..\images\minus.png'>
-                                    <?php
-                                }
-//                                echo $katVO->getName() . " (" . $katVO->getId() . ")";
-                                if ($katVO->getId() == $katIdAnzeige ?? '' ?? '') {
-//                                    echo '<span class="label label-danger">'.$katVO->getName().'</span>';
-                                    echo '<span style="background-color: #00ff00;">' . $katVO->getName() . '</span>';
-                                } else {
-                                    echo $katVO->getName();
-                                }
-                                ?>
-                                &nbsp;&nbsp;
-                                <a href='kategorieAendern.php?id=<?php echo $katVO->getId(); ?>&eventId=<?php echo $email ?? ''; ?>'>
-                                    <span class="glyphicon glyphicon-edit"></span></a>
-                                <a href="" onClick="confirmDelete('<?php echo $katVO->getId(); ?>', '<?php echo $email ?? ''; ?>');
-                                                return false;" id="a<?php echo $katVO->getId(); ?>">
-                                    <span class="glyphicon glyphicon-remove"></span></a>
-                                <a href='kategorieHinzufuegen.php?par=neuInHierarchie&beforeId=<?php echo $katVO->getId(); ?>&eventId=<?php echo $email ?? ''; ?>&insertLevel=<?php echo ($katVO->getLevel() + 1); ?>'>
-                                    <span class="glyphicon glyphicon-plus-sign"></span></a>&nbsp;&nbsp;
-                                <a href='kategorieInfoAendern.php?id=<?php echo $katVO->getId(); ?>&eventId=<?php echo $email ?? ''; ?>'>
-                                    <span class="glyphicon glyphicon-info-sign"></span></a>&nbsp;&nbsp;&nbsp;&nbsp;
-                                <?php
-                                if ($katVO->getInfo() != NULL) {
-                                    echo "(" . substr($katVO->getInfo(), 0, 40) . " ... )";
-                                }
-                                if ($i === (sizeof($kategorieArray) - 1)) {
-                                    echo"<tr><td>";
-                                    ?>
-                                    <a href='kategorieHinzufuegen.php?par=neuInHierarchie&beforeId=0&eventId=<?php echo $email ?? ''; ?>&insertLevel=0'>
-                                        <img src='..\images\neu.png'>Neue Hauptkategorie
-                                        <span class="glyphicon glyphicon-plus-sign"></span></a>
-                                        <?php
-                                        echo"</tr></td>";
-                                    }
-                                    echo"</td></tr>";
-                                    ?>
-                                    <?php
-                                }
-                            }
-                            ?>
-                    </table>
-
-
+            <ul>
+                @foreach($conversations as $conversation)
+                    @include('conversation-partial', ['conversation' => $conversation])
+                @endforeach
+            </ul>
         </div>
     </div>
 </div>
