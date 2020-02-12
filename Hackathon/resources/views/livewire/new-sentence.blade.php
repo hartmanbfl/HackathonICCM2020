bc51f4bd987f2da8c52a8bc2fa89cd49b0a9e935
Jeff Gage <jgage@gemedot.com>
2020-02-12 15:00:50 +0100

    cancel on new works

--- Hackathon/resources/views/livewire/new-sentence.blade.php
+++ Hackathon/resources/views/livewire/new-sentence.blade.php
@@ -1,11 +1,12 @@
 <li>
     @if(!$added)
-        @if($button)
-            <a href="#" wire:click="showInput" class="addThread">Add Thread</a>
-        @else
-            <input type="text" wire:model="sentence" wire:keydown.enter="add"> <button wire:click="add">Add</button>
-        @endif
+    @if($button)
+    <a href="#" wire:click="showInput" class="addThread">Add Thread</a>
     @else
-        {{$sentence}}
+    <input type="text" wire:model="sentence" wire:keydown.enter="add"> <button wire:click="add">Add</button> <button
+        wire:click="cancel">Cancel</button>
+    @endif
+    @else
+    {{$sentence}}
     @endif
 </li>
