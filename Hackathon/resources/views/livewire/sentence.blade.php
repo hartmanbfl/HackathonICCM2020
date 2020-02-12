873ed4b9f5913667f058df3ce005313a7491231d
Jeff Gage <jgage@gemedot.com>
2020-02-12 16:44:05 +0100

    A few cosmetic changes

--- Hackathon/resources/views/livewire/sentence.blade.php
+++ Hackathon/resources/views/livewire/sentence.blade.php
@@ -1,25 +1,26 @@
 <div>
     @if(!$deleted)
-        @if(!$deleteConfirm)
-            @if($editMode)
-                <input type="text" wire:model="sentence" wire:keydown.enter="save"> 
-                <button wire:click="save">Save</button>
-                <button wire:click="cancel">Cancel</button>
-            @else
-            {{-- Any clue how to toggle the symbol? --}}
-            {{-- <i class="{{($toggle ? "fa-chevron-right" : "n")}} fas icn"></i> {{$sentence}} <a href="#" wire:click="edit">Edit</a> <a href="#" wire:click="delete">Remove</a> --}}
-                @if(!$hasChildren)
-                    <i class="fa-chevron-down fas icn"></i>
-                @else
-                    <i class="fa-chevron-right fas icn"></i><i class="fa-chevron-down children fas icn hide"></i>
-                @endif
+    @if(!$deleteConfirm)
+    @if($editMode)
+    <input type="text" wire:model="sentence" wire:keydown.enter="save">
+    <button wire:click="save">Save</button>
+    <button wire:click="cancel">Cancel</button>
+    @else
 
-                {{$sentence}} &nbsp;<a class="fa-pen-square fas general-icon" wire:click="edit"></a>  &nbsp;<i class="fa-times-circle fas general-icon caution" wire:click="delete"></i>
-            @endif
-        @else
-            {{$sentence}}
+    @if(!$hasChildren)
+    <i class="fa-chevron-down fas icn"></i>
+    @else
+    <i class="fa-chevron-right fas icn"></i><i class="fa-chevron-down children fas icn hide"></i>
+    @endif
 
-            <button wire:click="deleteConfirmation(true)">Yes, delete</button> <button wire:click="deleteConfirmation(false)">No, keep this sentence</button>
-        @endif
+    {{$sentence}} &nbsp;<a class="fa-pen-square fas gIcon" wire:click="edit"></a> &nbsp;<i
+        class="fa-trash-alt fas gIcon caution" wire:click="delete"></i>
+    @endif
+    @else
+    {{$sentence}}
+
+    <button wire:click="deleteConfirmation(true)">Yes, delete</button> <button
+        wire:click="deleteConfirmation(false)">No, keep this sentence</button>
+    @endif
     @endif
 </div>
--- Hackathon/resources/views/welcome.blade.php
+++ Hackathon/resources/views/welcome.blade.php
@@ -1,56 +1,58 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Gospel Conversations</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <title>Gospel Conversations</title>
 
-         <!-- Styles -->
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+</head>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
             @endif
+            @endauth
+        </div>
+        @endif
 
-            <div class="content">
-                <div class="title">
-                    Gospel Conversations
-                </div>
-                <div class="subtitle m-b-md">
-                    Thread Manager
-                </div>
-                <div class="paragraph">
-                    The Gospel Conversations voice assistant skill 
-                    acts as a sparing partner to let you
-                    practice sharing your faith. Alexa asks you a 
-                    question, you respond, it responds to you, 
-                    and now you’re having a conversation. If you 
-                    say something where it doesn’t have a programmed 
-                    response, then it asks if you would like to hear 
-                    one where it has a response, and now it is 
-                    training you. <br /><br />
-                    Available soon for Amazon Alexa.
-                </div>
-                <img src="..\images\amazonalexa.png" />
-                
-{{-- 
+        <div class="content">
+            <div class="title">
+                Gospel Conversations
+            </div>
+            <div class="subtitle m-b-md">
+                Conversation Manager
+            </div>
+            <div class="paragraph">
+                The Gospel Conversations voice assistant skill
+                acts as a sparing partner to let you
+                practice sharing your faith. Alexa asks you a
+                question, you respond, it responds to you,
+                and now you’re having a conversation. If you
+                say something where it doesn’t have a programmed
+                response, then it asks if you would like to hear
+                one where it has a response, and now it is
+                training you. <br /><br />
+                Available soon for Amazon Alexa.
+            </div>
+            <img src="..\images\amazonalexa.png" />
+
+            {{--
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
@@ -61,7 +63,8 @@
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
                 </div> --}}
-            </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
