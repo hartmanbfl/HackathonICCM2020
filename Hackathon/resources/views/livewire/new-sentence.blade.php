ca3d72707916535eb5b1e7633a2f2d6f449fc7d8
Ben Hartman <bhartman@gemedot.com>
2020-02-13 09:55:47 +0100

    Modify indentation for blade files

--- Hackathon/resources/views/livewire/new-sentence.blade.php
+++ Hackathon/resources/views/livewire/new-sentence.blade.php
@@ -1,14 +1,14 @@
 <li>
-    @if(!$added)
-    @if($button)
-    {{-- <i class="fa-add fas gIcon" wire:click="showInput"></i> --}}
-    <a href="#" wire:click="showInput" class="addThread">Add Thread</a>
-    @else
-    <input class="tEntry" type="text" wire:model="sentence" wire:keydown.enter="add">
-    <i class="fa-save fas gIcon" wire:click="add"></i> &nbsp;
-    <i class="fa-window-close fas gIcon caution" wire:click="cancel"></i>
-    @endif
-    @else
-    {{$sentence}}
-    @endif
+   @if(!$added)
+     @if($button)
+       {{-- <i class="fa-add fas gIcon" wire:click="showInput"></i> --}}
+       <a href="#" wire:click="showInput" class="addThread">Add Thread</a>
+     @else
+       <input class="tEntry" type="text" wire:model="sentence" wire:keydown.enter="add">
+       <i class="fa-save fas gIcon" wire:click="add"></i> &nbsp;
+       <i class="fa-window-close fas gIcon caution" wire:click="cancel"></i>
+     @endif
+   @else
+     {{$sentence}}
+   @endif
 </li>
